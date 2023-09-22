4ad680d9f5c80e4e23cec57f3d36f6c0e136c6fc
Michael Nielson <michael.nielson@collectivemedicaltech.com>
2023-09-22 10:32:09 -0600

    initial commit

---
+++ src/main/java/com/example/openrewritegradle7issue/OpenrewriteGradle7IssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.openrewritegradle7issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenrewriteGradle7IssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenrewriteGradle7IssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openrewritegradle7issue/OpenrewriteGradle7IssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openrewritegradle7issue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenrewriteGradle7IssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
